3f2d01d59c0faa1cb0c275c1f736050da5b70bcb
prashant <prashantranjan816@gmail.com>
2018-07-22 16:04:46 +0530

    Commit from terminal again

---
+++ src/main/java/in/zipgo/repotest3/App.java
@@ -0,0 +1,28 @@
+package in.zipgo.repotest3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class App 
+{
+   @Test
+	public void abc() {
+		System.out.println("ABC running...");
+		
+	}
+   
+   
+   
+   @Test
+   public void abc1() {
+	   System.out.println("ABC1 running...");
+	   
+	   WebDriver driver = new ChromeDriver();
+	   driver.get("http://www.google.com");
+	   driver.quit();
+	   System.out.println("Browser has close");
+	   
+   }
+	
+}
