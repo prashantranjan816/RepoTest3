0dbfe0cc4bce6305f0591e0dbbb6804555ef97fc
prashantranjan816 <prashantranjan816@gmail.com>
2018-07-23 11:41:01 +0530

    Commit from Terminal

--- src/main/java/in/zipgo/repotest3/App.java
+++ src/main/java/in/zipgo/repotest3/App.java
@@ -26,6 +26,7 @@ public class App
 	   System.out.println("Browser has close");
 	   System.out.println("Browser has close");
 	   System.out.println("Browser has close");
+	   System.out.println("Browser has close");
 	   
    }
 	
